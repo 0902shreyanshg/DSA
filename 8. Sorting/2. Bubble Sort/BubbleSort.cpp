1574e4ebc867b4c009e99d14204127d411ccbaaf
Shreyansh <0902shreyanshg@gmail.com>
2022-09-07 13:08:45 +0530

    Updating bubble sort

--- 8. Sorting/2. Bubble Sort/BubbleSort.cpp
+++ 8. Sorting/2. Bubble Sort/BubbleSort.cpp
@@ -5,14 +5,29 @@
 
 using namespace std;
 
+// TIME COMPLEXITY: O(n^2)
+// BEST CASE: already sorted, O(n): see below the bool swapped
+// WORST CASE: reverse sorted
+
+// SPACE COMPLEXITY: only variables, constant space so, O(1)
+
 void bubbleSort(vector<int> &arr, int n)
 {
+    bool swapped = false; // FOR OPTIMIZATION, O(n)
+
     for (int i = 0; i < n - 1; i++) // for(int i = 1; i < n; i++)
+    // for round 1 to n-1
     {
         for (int j = 0; j < n - i - 1; j++) // for(int j = 0; j = n - 1; j++)
         {
             if (arr[j] > arr[j + 1])
                 swap(arr[j], arr[j + 1]);
+            swapped = true;
+        }
+        if (swapped == false)
+        {
+            // already sorted
+            break;
         }
     }
 }
